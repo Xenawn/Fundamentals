3009473197328555b30230426a269bd4e9546630
Xenawn <codeparadoxist@gmail.com>
2026-01-08 15:52:50 +0900

    [Chore] 정리

--- Assets/Survivalist/StarterAssets/Editor/ThirdPersonStarterAssetsDeployMenu.cs
+++ Assets/Survivalist/StarterAssets/Editor/ThirdPersonStarterAssetsDeployMenu.cs
@@ -1,54 +1,54 @@
-﻿using System.Linq;
-using UnityEditor;
-using UnityEngine;
+﻿//using System.Linq;
+//using UnityEditor;
+//using UnityEngine;
 
-namespace StarterAssets
-{
-    public partial class StarterAssetsDeployMenu : ScriptableObject
-    {
-        // prefab paths
-        private const string ThirdPersonPrefabPath = "/ThirdPersonController/Prefabs/";
-        private const string PlayerArmaturePrefabName = "PlayerArmature";
+//namespace StarterAssets
+//{
+//    public partial class StarterAssetsDeployMenu : ScriptableObject
+//    {
+//        // prefab paths
+//        private const string ThirdPersonPrefabPath = "/ThirdPersonController/Prefabs/";
+//        private const string PlayerArmaturePrefabName = "PlayerArmature";
 
-#if STARTER_ASSETS_PACKAGES_CHECKED
-        /// <summary>
-        /// Check the Armature, main camera, cinemachine virtual camera, camera target and references
-        /// </summary>
-        [MenuItem(MenuRoot + "/Reset Third Person Controller Armature", false)]
-        static void ResetThirdPersonControllerArmature()
-        {
-            var thirdPersonControllers = FindObjectsOfType<ThirdPersonController>();
-            var player = thirdPersonControllers.FirstOrDefault(controller => controller.GetComponent<Animator>() && controller.CompareTag(PlayerTag));
-            GameObject playerGameObject;
+//#if STARTER_ASSETS_PACKAGES_CHECKED
+//        /// <summary>
+//        /// Check the Armature, main camera, cinemachine virtual camera, camera target and references
+//        /// </summary>
+//        [MenuItem(MenuRoot + "/Reset Third Person Controller Armature", false)]
+//        static void ResetThirdPersonControllerArmature()
+//        {
+//            var thirdPersonControllers = FindObjectsOfType<ThirdPersonController>();
+//            var player = thirdPersonControllers.FirstOrDefault(controller => controller.GetComponent<Animator>() && controller.CompareTag(PlayerTag));
+//            GameObject playerGameObject;
 
-            // player
-            if (player == null)
-                HandleInstantiatingPrefab(StarterAssetsPath + ThirdPersonPrefabPath,
-                    PlayerArmaturePrefabName, out playerGameObject);
-            else
-                playerGameObject = player.gameObject;
+//            // player
+//            if (player == null)
+//                HandleInstantiatingPrefab(StarterAssetsPath + ThirdPersonPrefabPath,
+//                    PlayerArmaturePrefabName, out playerGameObject);
+//            else
+//                playerGameObject = player.gameObject;
 
-            // cameras
-            CheckCameras(ThirdPersonPrefabPath, playerGameObject.transform);
-        }
+//            // cameras
+//            CheckCameras(ThirdPersonPrefabPath, playerGameObject.transform);
+//        }
 
-        [MenuItem(MenuRoot + "/Reset Third Person Controller Capsule", false)]
-        static void ResetThirdPersonControllerCapsule()
-        {
-            var thirdPersonControllers = FindObjectsOfType<ThirdPersonController>();
-            var player = thirdPersonControllers.FirstOrDefault(controller => !controller.GetComponent<Animator>() && controller.CompareTag(PlayerTag));
-            GameObject playerGameObject;
+//        [MenuItem(MenuRoot + "/Reset Third Person Controller Capsule", false)]
+//        static void ResetThirdPersonControllerCapsule()
+//        {
+//            var thirdPersonControllers = FindObjectsOfType<ThirdPersonController>();
+//            var player = thirdPersonControllers.FirstOrDefault(controller => !controller.GetComponent<Animator>() && controller.CompareTag(PlayerTag));
+//            GameObject playerGameObject;
 
-            // player
-            if (player == null)
-                HandleInstantiatingPrefab(StarterAssetsPath + ThirdPersonPrefabPath,
-                    PlayerCapsulePrefabName, out playerGameObject);
-            else
-                playerGameObject = player.gameObject;
+//            // player
+//            if (player == null)
+//                HandleInstantiatingPrefab(StarterAssetsPath + ThirdPersonPrefabPath,
+//                    PlayerCapsulePrefabName, out playerGameObject);
+//            else
+//                playerGameObject = player.gameObject;
 
-            // cameras
-            CheckCameras(ThirdPersonPrefabPath, playerGameObject.transform);
-        }
-#endif
-    }
-}
+//            // cameras
+//            CheckCameras(ThirdPersonPrefabPath, playerGameObject.transform);
+//        }
+//#endif
+//    }
+//}
