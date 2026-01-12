94527e7943aa82171ccf78e2460c7ed134557138
Xenawn <codeparadoxist@gmail.com>
2026-01-12 15:28:08 +0900

    [Chore] Switch to the Android Platform

---
+++ Assets/Scripts/FirebaseLogin.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using Firebase.Auth;
+using Firebase;
+
+public class FirebaseLogin : MonoBehaviour
+{
+   FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+       
+}
