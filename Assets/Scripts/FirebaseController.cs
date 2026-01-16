592864f374d7c8e13dce47fa7bcc7d84e89c7a21
Xenawn <codeparadoxist@gmail.com>
2026-01-16 23:15:15 +0900

    [Update] Firebase 로그인, 회원가입, 로그아웃 구현

--- Assets/Scripts/FirebaseController.cs
+++ Assets/Scripts/FirebaseController.cs
@@ -1,6 +1,13 @@
+using System.Net.Mail;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Firebase;
+using Firebase.Auth;
+using System;
+using System.Threading.Tasks;
+using Firebase.Extensions;
+
 
 public class FirebaseController : MonoBehaviour
 {
@@ -8,6 +15,14 @@ public class FirebaseController : MonoBehaviour
     public TMP_InputField loginEmail, loginPassword, signupEmail, signupPassword, signupCPassword, signupUserName,forgetPassEmail;
     public TextMeshProUGUI notif_Title_text, notif_msg_txt,profile_UserName_Text,profile_Email_Text;
     public Toggle rememberme;
+    Firebase.Auth.FirebaseAuth auth;
+    Firebase.Auth.FirebaseUser user;
+    bool isSiginIn = false;
+
+    public void Start()
+    {
+        InitializeFirebase();
+    }
     public void OpenLoginPanel()
     {
         loginPanel.SetActive(true);
@@ -18,16 +33,16 @@ public class FirebaseController : MonoBehaviour
 
     public void OpenSignUpPanel()
     {
-        loginPanel.SetActive(true);
-        signupPanel.SetActive(false);
+        loginPanel.SetActive(false);
+        signupPanel.SetActive(true);
         profilePanel.SetActive(false);
         forgetPasswordPanel.SetActive(false);
     }
     public void OpenProfilePanel()
     {
-        loginPanel.SetActive(true);
+        loginPanel.SetActive(false);
         signupPanel.SetActive(false);
-        profilePanel.SetActive(false);
+        profilePanel.SetActive(true);
         forgetPasswordPanel.SetActive(false);
     }
     public void OpenForgetPassPanel()
@@ -47,6 +62,7 @@ public class FirebaseController : MonoBehaviour
         }
 
         //Do Login
+        SignInUser(loginEmail.text,loginPassword.text);
     }
 
     public void SignUpUser()
@@ -58,6 +74,7 @@ public class FirebaseController : MonoBehaviour
         }
 
         // Do signUp
+        CreateUser(signupEmail.text, signupPassword.text, signupUserName.text);
     }
 
     public void forgetPass()
@@ -86,9 +103,151 @@ public class FirebaseController : MonoBehaviour
 
     public void LogOut()
     {
+        auth.SignOut();
         profilePanel.SetActive(false);
         profile_UserName_Text.text = "";
         profile_Email_Text.text = "";
         OpenLoginPanel();
+        isSiginIn = false;
+        isSigned = false;
+    }
+
+    public void CreateUser(string email, string password, string Username)
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                IsValidPassword(password);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.AuthResult result = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                result.User.DisplayName, result.User.UserId);
+            UpdateUserProfile(Username);
+        });
+
+        
+    }
+
+    public void SignInUser(string email, string password )
+    {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.AuthResult result = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                result.User.DisplayName, result.User.UserId);
+            profile_UserName_Text.text = "" + result.User.DisplayName;
+            profile_Email_Text.text = "" + result.User.Email;
+            OpenProfilePanel();
+        });
+    }
+
+    void InitializeFirebase()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+    }
+
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        if (auth.CurrentUser != user)
+        {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null
+                && auth.CurrentUser.IsValid();
+            if (!signedIn && user != null)
+            {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            user = auth.CurrentUser;
+            if (signedIn)
+            {
+                Debug.Log("Signed in " + user.UserId);
+                isSiginIn = true;
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        auth.StateChanged -= AuthStateChanged;
+        auth = null;
+    }
+
+    public void UpdateUserProfile(string userName)
+    {
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        if (user != null)
+        {
+            Firebase.Auth.UserProfile profile = new Firebase.Auth.UserProfile
+            {
+                DisplayName = userName,
+                PhotoUrl = new System.Uri("https://dummyimage.com/150"),
+            }; 
+            user.UpdateUserProfileAsync(profile).ContinueWith(task => {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("UpdateUserProfileAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("UpdateUserProfileAsync encountered an error: " + task.Exception);
+                    return;
+                }
+
+                Debug.Log("User profile updated successfully.");
+                showNotificationMessage("Alert", "Account Successfully Created");
+            });
+        }
+    }
+
+    bool IsValidPassword(string password)
+    {
+        if (password.Length < 6)
+        {
+            Debug.LogError("비밀번호는 최소 6자 이상이어야 합니다.");
+            showNotificationMessage("Password Failed","Please create a password with at least 6 characters.");
+            return false;
+        }
+
+        // 추가 검증 (선택)
+        // - 대문자, 소문자, 숫자 포함 등
+
+        return true;
+    }
+
+    bool isSigned = false;
+    private void Update()
+    {
+        if (isSiginIn)
+        {
+
+            if (!isSigned)
+            {
+                isSigned = true;
+                profile_UserName_Text.text = "" + user.DisplayName;
+                profile_Email_Text.text = "" + user.Email;
+                OpenProfilePanel();
+            }
+        }
     }
 }
