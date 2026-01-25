16e5983b02771265101074ef0559d6f31aefe590
Xenawn <codeparadoxist@gmail.com>
2026-01-26 08:52:19 +0900

    [Update] Input System+ Character controller
    New Input System과 Character controller를 이용하여
    캐릭터 움직임 완성

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float playerSpeed = 5.0f;
+    [SerializeField]
+    private float jumpHeight = 1.5f;
+    [SerializeField]
+    private float gravityValue = -9.81f;
+
+    public CharacterController controller;
+    private Player playerInput;
+
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public InputActionReference moveAction;
+   
+
+    private void Awake()
+    {
+        playerInput = new Player();
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+   
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+
+        if (groundedPlayer && playerVelocity.y<0)
+        {
+                playerVelocity.y = 0f;
+        }
+
+        // Read input
+        Vector2 movementInput = playerInput.PlayerMain.Move.ReadValue<Vector2>();
+        Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
+        move = Vector3.ClampMagnitude(move, 1f);
+
+        if (move != Vector3.zero)
+            transform.forward = move;
+
+        // Jump using WasPressedThisFrame()
+        if (groundedPlayer && playerInput.PlayerMain.Jump.triggered)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
+        }
+
+        // Apply gravity
+        playerVelocity.y += gravityValue * Time.deltaTime;
+
+        // Move
+        Vector3 finalMove = move * playerSpeed + Vector3.up * playerVelocity.y;
+        controller.Move(finalMove * Time.deltaTime);
+    }
+}
